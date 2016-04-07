ac741fa2aca512e79c4497c23844c112115968d7
constantin <constantin@OFICINA303-PC>
2016-04-06 22:21:34 -0300

    Add project files.

---
+++ PowerSystemPlanningGUI/PowerSystemPlanningGUI_MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerSystemPlanningGUI
+{
+    public partial class PowerSystemPlanningGUI_MainWindow : Form
+    {
+        public PowerSystemPlanningGUI_MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

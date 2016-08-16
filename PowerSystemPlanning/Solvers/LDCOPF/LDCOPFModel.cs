3563f9186defae09bf3a21f3b4bbde51eac07e9c
Constantin Velásquez <cvelasquez.gnu@gmail.com>
2016-08-16 00:09:33 -0300

    minor advances on LDC opf, now correctly included

---
+++ PowerSystemPlanning/Solvers/LDCOPF/LDCOPFModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerSystemPlanning.Solvers.LDCOPF
+{
+    public class LDCOPFModel
+    {
+        List<DurationBlock> _DurationBlocks;
+
+        public List<DurationBlock> DurationBlocks
+        {
+            get
+            {
+                return _DurationBlocks;
+            }
+
+            set
+            {
+                _DurationBlocks = value;
+            }
+        }
+    }
+
+    public struct DurationBlock
+    {
+        double Duration;
+        double LoadMultiplier;
+    }
+}
---
+++ PowerSystemPlanningWpfApp/LDC/OptimizeOPFLDC.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PowerSystemPlanningWpfApp.LDC
+{
+    /// <summary>
+    /// Interaction logic for OptimizeOPFLDC.xaml
+    /// </summary>
+    public partial class OptimizeOPFLDC : Window
+    {
+
+
+        public OptimizeOPFLDC()
+        {
+            InitializeComponent();
+        }
+    }
+}
